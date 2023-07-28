bd372450d09c0fd09bb6687c81a63c552980eed4
cryshado <a.gapak@everx.dev>
2023-07-28 18:38:04 -0400

    rm duplicate vars in tests

--- src/scheme/tests.rs
+++ src/scheme/tests.rs
@@ -20,9 +20,7 @@ use ton_types::{read_boc, write_boc};
 use super::*;
 use hex_literal::hex;
 
-#[test]
-fn test_temp() {
-    const DESCSTR: &str = r#"
+static DESCSTR: &'static str = r#"
 Once upon a time, there was a little kitten named Whiskers. Whiskers was the runt of the
 litter and always had trouble keeping up with his siblings. His mother loved him dearly,
 but he often felt left out during playtime.
@@ -41,6 +39,8 @@ Despite his humble beginnings, Whiskers learned that he could achieve great thin
 following his passions and staying true to himself. And he knew that no matter how
 small he may seem, he had a big heart and a lot to offer the world."#;
 
+#[test]
+fn test_temp() {
     let mut builder = BuilderData::new();
     builder.append_u64(u64::MAX).unwrap();
     builder_store_string_ref(&mut builder, DESCSTR).unwrap();
@@ -55,26 +55,6 @@ small he may seem, he had a big heart and a lot to offer the world."#;
 
 #[test]
 fn test_tvm_contract() {
-    const DESCSTR: &str = r#"
-Once upon a time, there was a little kitten named Whiskers. Whiskers was the runt of the
-litter and always had trouble keeping up with his siblings. His mother loved him dearly,
-but he often felt left out during playtime.
-
-One day, while exploring the yard, Whiskers stumbled upon a butterfly. Mesmerized by its
-beauty, Whiskers chased the butterfly around the garden, forgetting all about his troubles.
-From that day on, Whiskers became fascinated with the natural world and spent all his time
-exploring and learning about the creatures that lived around him.
-
-As he grew older, Whiskers became known throughout the neighborhood for his knowledge of
-nature and his ability to make friends with all kinds of animals. His siblings may have
-been faster and stronger, but Whiskers had found his own special talent and became a
-beloved member of the community.
-
-Despite his humble beginnings, Whiskers learned that he could achieve great things by
-following his passions and staying true to himself. And he knew that no matter how
-small he may seem, he had a big heart and a lot to offer the world.
-"#;
-
     const CODEBOC: [u8; 27] = hex!("B5EE9C7241010101000C000014FF00F8008101008011A1EF0ED546");
     let code = &read_boc(&CODEBOC).unwrap().roots[0];
 
